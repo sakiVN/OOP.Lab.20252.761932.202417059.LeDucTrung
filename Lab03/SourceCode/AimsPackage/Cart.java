424affe92caa2f4e3cadbe681ccab2c43ee48ad2
Lê Đức Trung <trungthoncham@gmail.com>
2026-04-14 16:03:31 +0700

    Add method overloading for addDigitalVideoDisc (array, two params, varargs)

---
+++ Lab03/SourceCode/AimsPackage/Cart.java
@@ -0,0 +1,62 @@
+package AimsPackage;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+
+    private DigitalVideoDisc[] itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+    // Thêm DVD
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        } else {
+            System.out.println("The cart is almost full");
+        }
+    }
+    // Overload
+    public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+        for (int i = 0; i < dvdList.length; i++) {
+            addDigitalVideoDisc(dvdList[i]); // reuse method cũ
+        }
+    }
+
+    // Xóa DVD
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean found = false;
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                found = true;
+
+                // dồn mảng
+                for (int j = i; j < qtyOrdered - 1; j++) {
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                }
+
+                itemsOrdered[qtyOrdered - 1] = null;
+                qtyOrdered--;
+
+                System.out.println("The disc has been removed");
+                break;
+            }
+        }
+
+        if (!found) {
+            System.out.println("The disc was not found");
+        }
+    }
+
+    // Tính tổng tiền
+    public float totalCost() {
+        float total = 0;
+
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+
+        return total;
+    }
+}
