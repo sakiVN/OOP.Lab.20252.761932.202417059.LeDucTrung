e4d3075cce35ca7d3600fdee93adb6807ec7085c
Le Duc Trung <trungthoncham@gmail.com>
2026-04-24 04:54:59 +0700

    Adding minor changes
    Complete all other tasks like answer and diagram, as well as fixing directions.

---
+++ Lab03/AimsProject/SourceCode/All Source Code/hust/soict/hedspi/test/cart/CartTest.java
@@ -0,0 +1,34 @@
+package hust.soict.hedspi.test.cart;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class CartTest {
+    public static void main(String[] args) {
+
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin");
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // 🔹 In giỏ hàng
+        cart.print();
+
+        // 🔹 Tìm theo ID
+        System.out.println("\nSearch by ID:");
+        cart.searchById(2);
+
+        // 🔹 Tìm theo title
+        System.out.println("\nSearch by title:");
+        cart.searchByTitle("star");
+
+        // 🔹 test không tìm thấy
+        System.out.println("\nSearch not found:");
+        cart.searchByTitle("abc");
+    }
+}
